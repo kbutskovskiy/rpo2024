bc187c56c0bab7c3fbefcd3ca9ed39b7d3ef5748
kbutskovskiy <kbutskovskiy@mail.ru>
2024-04-17 16:19:26 +0300

    lab7 done

--- Backend/src/main/java/ru/iu3/backend/repository/UserRepository.java
+++ Backend/src/main/java/ru/iu3/backend/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package ru.iu3.backend.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.iu3.backend.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByToken(String token);
+    Optional<User> findByLogin(String login);
 }
