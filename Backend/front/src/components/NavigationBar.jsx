1e15425146834425cdb950c8b6e53150004e133d
kbutskovskiy <kbutskovskiy@mail.ru>
2024-04-18 17:04:38 +0300

    navigation bar started

--- Backend/front/src/components/NavigationBar.jsx
+++ Backend/front/src/components/NavigationBar.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
-const NavigationBar = () => {
-  const navigate = useNavigate();
+class NavigationBar extends React.Component {
 
-  return (
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href={"/home"}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate("Home", { replace: true })}>Another Home</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-  );
-};
+  constructor(props) {
+    super(props);
+  }
 
-export default NavigationBar;
+  render() {
+    return (
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand><FontAwesomeIcon icon={faHome} />{' '}My RPO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link>Home</Nav.Link>
+              <Nav.Link>Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+    );
+  }
+}
+
+export default  NavigationBar;
